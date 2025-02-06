ec971f3142068ebebec2ab7788703b1ab8e10a3f
bax082024 <bax082024@gmail.com>
2025-02-06 15:19:49 +0100

    first commit

---
+++ UnitConverter.cpp
@@ -0,0 +1,19 @@
+// UnitConverter.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+using namespace std;
+
+// Functions
+void showMenu();
+void metersToFeet();
+void feetToMeters();
+void kilometersToMiles();
+void milesToKilometers();
+void celsiusToFahrenheit();
+void fahrenheitToCelsius();
+void kilogramsToPounds();
+void poundsToKilograms();
+void litersToGallons();
+void gallonsToLiters();
