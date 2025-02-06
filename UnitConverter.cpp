86b61a3ea76b6e21f91a5fb22a92205688a25312
bax082024 <bax082024@gmail.com>
2025-02-06 15:54:56 +0100

    added switch with choices

--- UnitConverter.cpp
+++ UnitConverter.cpp
@@ -17,3 +17,57 @@ void kilogramsToPounds();
 void poundsToKilograms();
 void litersToGallons();
 void gallonsToLiters();
+
+int main() {
+	int choice;
+
+	do {
+		showMenu();
+		cout << "Enter your choice: ";
+		cin >> choice;
+
+		if (cin.fail()) {
+			cin.clear();
+			cin.ignore(1000, '\n');
+			cout << "Invalid input. Please enter a number." << endl;
+		}
+		else {
+			switch (choice) {
+			case 1:
+				metersToFeet();
+				break;
+			case 2:
+				feetToMeters();
+				break;
+			case 3:
+				kilometersToMiles();
+				break;
+			case 4:
+				milesToKilometers();
+				break;
+			case 5:
+				celsiusToFahrenheit();
+				break;
+			case 6:
+				fahrenheitToCelsius();
+				break;
+			case 7:
+				kilogramsToPounds();
+				break;
+			case 8:
+				poundsToKilograms();
+				break;
+			case 9:
+				litersToGallons();
+				break;
+			case 10:
+				gallonsToLiters();
+				break;
+			case 11:
+				cout << "Exiting program..." << endl;
+				break;
+			default:
+				cout << "Invalid choice. Please enter a number between 1 and 11." << endl;
+				break;
+			}
+		}
