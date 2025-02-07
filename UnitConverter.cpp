82fff80a85618b37dc6334504e4f9d5a730ea6e6
bax082024 <bax082024@gmail.com>
2025-02-07 14:56:16 +0100

    added logic to convert roman numerals to decimal

--- UnitConverter.cpp
+++ UnitConverter.cpp
@@ -1670,6 +1670,27 @@ void decimalToRoman() {
 	} while (choice == 'y' || choice == 'Y');
 }
 
+int convertRomanToDecimal(const string& roman) {
+	unordered_map<char, int> romanValues = {
+		{'I', 1}, {'V', 5}, {'X', 10}, {'L', 50}, {'C', 100},
+		{'D', 500}, {'M', 1000}
+	};
+
+	int total = 0;
+	int prevValue = 0;
+
+	for (int i = roman.length() - 1; i >= 0; --i) {
+		int currentValue = romanValues[roman[i]];
+		if (currentValue < prevValue) {
+			total -= currentValue;
+		}
+		else {
+			total += currentValue;
+		}
+		prevValue = currentValue;
+	}
+	return total;
+}
 
 
 
